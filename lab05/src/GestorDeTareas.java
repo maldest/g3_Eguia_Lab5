5a66fee0b31b1814b82e9de37ee5ff9352195ceb
maldest <andre.eguia01@gmail.com>
2025-05-06 01:32:51 -0500

    Llamamos a las librerias que usaremos y llamamos al nodo (head)

--- lab05/src/GestorDeTareas.java
+++ lab05/src/GestorDeTareas.java
@@ -1,4 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
 
 public class GestorDeTareas<T> {
-
+    private Node<T> head;             // Este es el primer nodo de nuestra lista enlazada de tareas
+    private List<T> tareasCompletadas = new ArrayList<>();
 }
