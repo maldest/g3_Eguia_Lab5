081d63c816862f7b9b9ed99bd3107fcc785b8b3b
maldest <andre.eguia01@gmail.com>
2025-05-06 01:33:57 -0500

    Creamos el constructor y inicializamos la lista como null

--- lab05/src/GestorDeTareas.java
+++ lab05/src/GestorDeTareas.java
@@ -4,4 +4,14 @@ import java.util.List;
 public class GestorDeTareas<T> {
     private Node<T> head;             // Este es el primer nodo de nuestra lista enlazada de tareas
     private List<T> tareasCompletadas = new ArrayList<>();
+    
+ // Esta lista normal de Java va a guardar las tareas que ya se terminaron
+
+    // Este es el constructor ->  cuando creamos el gestor, no hay tareas aún
+    public GestorDeTareas() {
+        head = null; // La lista enlazada empieza vacía
+        tareasCompletadas = new ArrayList<>(); // Creamos una lista vacía para las tareas completadas
+    
+}
+
 }
