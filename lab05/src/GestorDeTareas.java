14df2ef90ada088356c0c6f01b2cb5e118fb059e
maldest <andre.eguia01@gmail.com>
2025-05-06 01:42:20 -0500

    Agregamos el metodo para poder crear una tarea al final de la lista enlazada

--- lab05/src/GestorDeTareas.java
+++ lab05/src/GestorDeTareas.java
@@ -4,7 +4,7 @@ import java.util.List;
 public class GestorDeTareas<T> {
     private Node<T> head;             // Este es el primer nodo de nuestra lista enlazada de tareas
     private List<T> tareasCompletadas = new ArrayList<>();
-    
+
  // Esta lista normal de Java va a guardar las tareas que ya se terminaron
 
     // Este es el constructor ->  cuando creamos el gestor, no hay tareas aún
@@ -13,5 +13,22 @@ public class GestorDeTareas<T> {
         tareasCompletadas = new ArrayList<>(); // Creamos una lista vacía para las tareas completadas
     
 }
+
+
+public void agregarTarea(T tarea) {
+    Node<T> nuevoNodo = new Node<>(tarea); // Primero creamos un nuevo nodo que guarde la tarea
+
+ if (head == null) {
+    // Si la lista estaba vacía , este nodo será el primero
+    head = nuevoNodo;
+    } else {
+        // Si ya hay tareas -> recorremos hasta encontrar el ultimo nodo
+        Node<T> actual = head;
+        while (actual.next != null) {
+        actual = actual.next; // Seguimos avanzando hasta el ultimo nodo
+        }
+        // Cuando encontramos el último, conectamos su "next" al nuevo nodo
+        actual.next = nuevoNodo;
+    }
 
 }
